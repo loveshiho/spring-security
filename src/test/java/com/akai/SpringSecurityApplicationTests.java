8f404e68540ee47c8626739a0dd1d687dbc22853
loveshiho <1822881521@qq.com>
2024-05-13 17:41:58 +0800

    SpringSecurity——>密码加密算法

--- src/test/java/com/akai/SpringSecurityApplicationTests.java
+++ src/test/java/com/akai/SpringSecurityApplicationTests.java
@@ -1,5 +1,6 @@
 package com.akai;
 
+import com.baomidou.mybatisplus.core.toolkit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -21,5 +22,19 @@ class SpringSecurityApplicationTests {
         // $2a$10$FUWa2cbArUlWDuDsfsyGXuRL75BGd81GEBk79IH2U3QWIzcKxCOgG
         boolean match = encoder.matches("shiho", result);
         System.out.println(match);  // true
+    }
+
+    @Test
+    void testPassword() {
+
+        // 工作因子，默认值是10，最小值是4，最大值是31，值越大运算速度越慢
+        PasswordEncoder encoder = new BCryptPasswordEncoder(4);
+        // 明文："password"
+        // 密文：result，即使明文密码相同，每次生成的密文也不一致
+        String result = encoder.encode("password");
+        System.out.println(result);
+
+        // 密码校验
+        Assert.isTrue(encoder.matches("password", result), "密码不一致");
     }
 }
